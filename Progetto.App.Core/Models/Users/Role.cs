b3fb2198e3ea17475bd733608b29458d8dfa465d
Ziocash <31541164+Ziocash@users.noreply.github.com>
2024-04-24 13:23:17 +0200

    Authorization
    [+] Added authorization handler
    [+] Added authorization requirement for admin user

---
+++ Progetto.App.Core/Models/Users/Role.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progetto.App.Core.Models.Users;
+
+public enum Role
+{
+    Admin,
+    PremiumUser,
+    User
+}
+
+public static class ClaimName
+{
+    public const string Role = "Role";
+
+    public const string FirstName = "FirstName";
+
+    public const string LastName = "LastName";
+}
---
+++ Progetto.App.Core/Security/Policies/IsAdmin.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Progetto.App.Core.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progetto.App.Core.Security.Policies;
+
+public class IsAdmin : IAuthorizationRequirement { }
+
+public class IsAdminAuthorizationHandler : AuthorizationHandler<IsAdmin>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsAdmin requirement)
+    {
+        if (context.User.HasClaim(ClaimName.Role, $"{Role.Admin}"))
+            context.Succeed(requirement);
+        
+        return Task.FromResult(context);
+    }
+}
