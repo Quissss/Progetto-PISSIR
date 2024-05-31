1dd0db955b7c7deda1bb01276cdf9f71caa26e2c
Luca Benetti <163659760+Quissss@users.noreply.github.com>
2024-06-01 00:08:53 +0200

    controller parkingslot

--- Progetto.App.Core/Repositories/ParkingRepository.cs
+++ Progetto.App.Core/Repositories/ParkingRepository.cs
@@ -1,4 +1,5 @@
-﻿using Progetto.App.Core.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Progetto.App.Core.Data;
 using Progetto.App.Core.Models;
 using System;
 using System.Collections.Generic;
@@ -11,4 +12,6 @@ namespace Progetto.App.Core.Repositories;
 public class ParkingRepository : GenericRepository<Parking>
 {
     public ParkingRepository(ApplicationDbContext context) : base(context) { }
+ 
 }
+
---
+++ Progetto.App/Controllers/ParkingSlotController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Progetto.App.Core.Models;
+using Progetto.App.Core.Repositories;
+
+
+namespace Progetto.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParkingSlotController : ControllerBase
+    {
+        private readonly ParkingSlotRepository _parkingSlotRepository;
+
+        public ParkingSlotController(ParkingSlotRepository parkingRepository)
+        {
+            _parkingSlotRepository = parkingRepository;
+        }
+
+        // Metodo per ottenere lo stato di occupazione del parcheggio
+        [HttpGet("{parkingId}/occupancy")]
+        public async Task<ActionResult<ParkingSlot>> GetParkingStatus(int parkingId)
+        {
+            var parkSingle = await _parkingSlotRepository.GetByIdAsync(parkingId);
+            if (parkSingle == null)
+            {
+                return NotFound();
+            }
+            return Ok(parkSingle.Status);
+        }
+
+        // Metodo per aggiornare lo stato di occupazione di uno slot di parcheggio
+        [HttpPut("slot/{slotId}/occupancy")]
+        public async Task<ActionResult<ParkingSlot>> UpdateParkingSlotOccupancy(int slotId, [FromBody] ParkSlotStatus status)
+        {
+            var parkSingle = await _parkingSlotRepository.GetByIdAsync(slotId);
+            parkSingle.Status = status;
+            await _parkingSlotRepository.UpdateAsync(parkSingle);
+
+            if (parkSingle == null)
+            {
+                return NotFound();
+            }
+            return Ok(parkSingle);
+        }
+
+    }
+}
