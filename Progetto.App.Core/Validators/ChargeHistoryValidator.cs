6c9afdca055266962823a45850842b0497f20427
Anton Iliev <akaantoninox@gmail.com>
2024-06-04 01:04:48 +0200

    Update ChargeHistoryValidator.cs

--- Progetto.App.Core/Validators/ChargeHistoryValidator.cs
+++ Progetto.App.Core/Validators/ChargeHistoryValidator.cs
@@ -17,9 +17,14 @@ public class ChargeHistoryValidator : AbstractValidator<ChargeHistory>
         RuleFor(ch => ch.EndDate)
             .NotEmpty().WithMessage("End Date is required");
         RuleFor(ch => ch.StartChargeLevel)
-            .NotEmpty().WithMessage("Start Charge Level is required");
+            .NotEmpty().WithMessage("Start Charge Level is required")
+            .LessThan(100).WithMessage("Start Charge Level must be less than 100")
+            .GreaterThanOrEqualTo(0).WithMessage("Start Charge Level must be greater than or equal to 0");
         RuleFor(ch => ch.EndChargeLevel)
-            .NotEmpty().WithMessage("End Charge Level is required");
+            .NotEmpty().WithMessage("End Charge Level is required")
+            .GreaterThan(0).WithMessage("End Charge Level must be greater than 0")
+            .LessThanOrEqualTo(100).WithMessage("End Charge Level must be less than or equal to 100")
+            .GreaterThanOrEqualTo(ch => ch.StartChargeLevel).WithMessage("End Charge Level must be greater than or equal to Start Charge Level");
         RuleFor(ch => ch.MWBotId)
             .NotEmpty().WithMessage("MWBot is required");
         RuleFor(ch => ch.UserId)
