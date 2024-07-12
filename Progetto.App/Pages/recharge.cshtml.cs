419185efb35211ab3b9365cff8a996020d144451
Luca Benetti <163659760+Quissss@users.noreply.github.com>
2024-07-12 18:19:43 +0200

    Add page For recharge

---
+++ Progetto.App/Pages/recharge.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Progetto.App.Core.Models;
+using Progetto.App.Core.Repositories;
+using System.Threading.Tasks;
+
+namespace Progetto.App.Pages
+{
+    public class rechargeModel : PageModel
+    {
+     
+       
+
+        public async Task OnGetAsync()
+        {
+
+        }
+    }
+}
