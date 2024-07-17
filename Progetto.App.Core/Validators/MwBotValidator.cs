557b5c6ad0c982a23f2e56c96668231584ad3079
Luca Benetti <163659760+Quissss@users.noreply.github.com>
2024-07-17 17:58:56 +0200

    Fix Validator e cchange dash

--- Progetto.App.Core/Validators/MwBotValidator.cs
+++ Progetto.App.Core/Validators/MwBotValidator.cs
@@ -19,5 +19,7 @@ public class MwBotValidator : AbstractValidator<MwBot>
             .NotEmpty().WithMessage("Battery Percentage is required")
             .LessThanOrEqualTo(100).WithMessage("Battery Percentage must be less than or equal to 100")
             .GreaterThanOrEqualTo(0).WithMessage("Battery Percentage must be greater than or equal to 0");
+        RuleFor(mw => mw.Status)
+            .IsInEnum().WithMessage("Status must be a valid MwBotStatus");
     }
 }
