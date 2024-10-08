ba9011fc80eee2ef62b957bb13b599c7f89b5246
Luca Benetti <163659760+Quissss@users.noreply.github.com>
2024-10-08 19:41:46 +0200

    Aggiunto Premium
    Aggiunto il pannello e api
    ma non funziona api

---
+++ Progetto.App/Pages/Premium.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Progetto.App.Pages
+{
+    public class PremiumModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
